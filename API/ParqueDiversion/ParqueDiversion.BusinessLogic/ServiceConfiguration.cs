f5b03e493aa4f74d33ecbcd4976f0360ddf6bfda
juans <125337384+jumAngie@users.noreply.github.com>
2023-05-16 14:52:59 -0600

    API ( Estructura 50%)
    * Agregue los paquetes

---
+++ API/ParqueDiversion/ParqueDiversion.BusinessLogic/ServiceConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParqueDiversion.BusinessLogic
+{
+    public static class ServiceConfiguration
+    {
+        public static void DataAccess(this IServiceCollection services, string connection)
+        {
+
+        }
+
+        public static void BusinessLogic(this IServiceCollection services)
+        {
+        }
+    }
+}
