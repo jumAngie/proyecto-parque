0984ec1a58975afd7571d0083cf9ef5f214d86e3
Angie Campos <angie.camposyc03@gmail.com>
2023-05-19 02:29:45 -0600

    CLIENTES REGISTRADOS REPO

--- API/ParqueDiversion/ParqueDiversion.DataAccess/Repositories/Parq/ClientesRegistradosRepository.cs
+++ API/ParqueDiversion/ParqueDiversion.DataAccess/Repositories/Parq/ClientesRegistradosRepository.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using ParqueDiversion.Entities.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ParqueDiversion.Entities.Entities;
 
 namespace ParqueDiversion.DataAccess.Repositories.Parq
 {
@@ -11,27 +14,59 @@ namespace ParqueDiversion.DataAccess.Repositories.Parq
     {
         public RequestStatus Delete(int id)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+            parametros.Add("@clre_ID", id, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<RequestStatus>(ScriptsDatabase.UDP_ClientesRegistrados_Delete, parametros, commandType: CommandType.StoredProcedure);
+
         }
 
         public VW_tbClientesRegistrados Find(int id)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+            parametros.Add("@clre_Id", id, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<VW_tbClientesRegistrados>(ScriptsDatabase.UDP_ClientesRegistrados_Find, parametros, commandType: CommandType.StoredProcedure);
+
         }
 
         public RequestStatus Insert(tbClientesRegistrados item)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+
+            parametros.Add("@clie_ID", item.clie_ID, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@clre_Usuario", item.clre_Usuario, DbType.String, ParameterDirection.Input);
+            parametros.Add("@clre_Email", item.clre_Email, DbType.String, ParameterDirection.Input);
+            parametros.Add("@clre_Clave", item.clre_Clave, DbType.String, ParameterDirection.Input);
+            parametros.Add("@clre_UsuarioCreador", item.clre_UsuarioCreador, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<RequestStatus>(ScriptsDatabase.UDP_ClientesRegistrados_Insert, parametros, commandType: CommandType.StoredProcedure);
+
         }
 
         public IEnumerable<VW_tbClientesRegistrados> List()
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+            return db.Query<VW_tbClientesRegistrados>(ScriptsDatabase.UDP_ClientesRegistrados_List, null, commandType: CommandType.StoredProcedure);
         }
 
         public RequestStatus Update(tbClientesRegistrados item)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+
+            parametros.Add("@clre_ID", item.clre_ID, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@clie_ID", item.clie_ID, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@clre_Usuario", item.clre_Usuario, DbType.String, ParameterDirection.Input);
+            parametros.Add("@clre_Email", item.clre_Email, DbType.String, ParameterDirection.Input);
+            parametros.Add("@clre_UsuarioModificador", item.clre_UsuarioModificador, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<RequestStatus>(ScriptsDatabase.UDP_ClientesRegistrados_Update, parametros, commandType: CommandType.StoredProcedure);
+
         }
     }
 }
