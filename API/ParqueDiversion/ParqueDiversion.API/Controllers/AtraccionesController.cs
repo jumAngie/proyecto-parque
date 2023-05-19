d7ac6b6009c2385afee038eddea06c3a1ca587c6
Angie Campos <angie.camposyc03@gmail.com>
2023-05-19 03:57:17 -0600

    8/8 CONTROLADORES

---
+++ API/ParqueDiversion/ParqueDiversion.API/Controllers/AtraccionesController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ParqueDiversion.API.Models;
+using ParqueDiversion.BusinessLogic.Services;
+using ParqueDiversion.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParqueDiversion.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AtraccionesController : Controller
+    {
+        private readonly ParqueServices _parqueServices;
+        private readonly IMapper _mapper;
+
+        public AtraccionesController(ParqueServices parqueServices, IMapper mapper)
+        {
+            _parqueServices = parqueServices;
+            _mapper = mapper;
+        }
+
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            var listado = _parqueServices.AtraccionesList();
+            return Ok(listado);
+        }
+
+        [HttpPost("Insert")]
+        public IActionResult Insert(AtraccionesViewModel item)
+        {
+            var listado = _mapper.Map<tbAtracciones>(item);
+            var result = _parqueServices.InsertarAtracciones(listado);
+            return Ok(result);
+        }
+
+
+        [HttpGet("Find/{id}")]
+        public IActionResult Edit(int id)
+        {
+            var listado = _parqueServices.FindAtracciones(id);
+            return Ok(listado);
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Edit(AtraccionesViewModel item)
+        {
+            var listado = _mapper.Map<tbAtracciones>(item);
+            var Result = _parqueServices.UpdateAtracciones(listado);
+            return Ok(Result);
+        }
+
+        [HttpPost("Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var listado = _parqueServices.BorrarAtracciones(id);
+            return Ok(listado);
+        }
+    }
+}
---
+++ API/ParqueDiversion/ParqueDiversion.API/Controllers/TicketClientesController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ParqueDiversion.API.Models;
+using ParqueDiversion.BusinessLogic.Services;
+using ParqueDiversion.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParqueDiversion.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketClientesController : Controller
+    {
+        private readonly ParqueServices _parqueServices;
+        private readonly IMapper _mapper;
+
+        public TicketClientesController(ParqueServices parqueServices, IMapper mapper)
+        {
+            _parqueServices = parqueServices;
+            _mapper = mapper;
+        }
+
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            var listado = _parqueServices.TicketClientesList();
+            return Ok(listado);
+        }
+
+        [HttpPost("Insert")]
+        public IActionResult Insert(TicketClienteViewModel item)
+        {
+            var listado = _mapper.Map<tbTicketsCliente>(item);
+            var result = _parqueServices.InsertarTicketClientes(listado);
+            return Ok(result);
+        }
+
+
+        [HttpGet("Find/{id}")]
+        public IActionResult Edit(int id)
+        {
+            var listado = _parqueServices.FindTicketClientes(id);
+            return Ok(listado);
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Edit(TicketClienteViewModel item)
+        {
+            var listado = _mapper.Map<tbTicketsCliente>(item);
+            var Result = _parqueServices.UpdateTicketClientes(listado);
+            return Ok(Result);
+        }
+
+        [HttpPost("Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var listado = _parqueServices.BorrarTicketClientes(id);
+            return Ok(listado);
+        }
+    }
+}
