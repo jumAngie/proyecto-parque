cfa95877456ea1e0d3d2c6224b4684b304615ff9
Angie Campos <angie.camposyc03@gmail.com>
2023-05-19 02:12:04 -0600

    cargo && regiones
    repos

--- API/ParqueDiversion/ParqueDiversion.DataAccess/Repositories/Parq/CargoRepository.cs
+++ API/ParqueDiversion/ParqueDiversion.DataAccess/Repositories/Parq/CargoRepository.cs
@@ -1,6 +1,9 @@
-﻿using ParqueDiversion.Entities.Entities;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using ParqueDiversion.Entities.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,27 +14,55 @@ namespace ParqueDiversion.DataAccess.Repositories.Parq
     {
         public RequestStatus Delete(int id)
         {
-            throw new NotImplementedException();
-        }
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+            parametros.Add("@carg_Id", id, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<RequestStatus>(ScriptsDatabase.UDP_Cargos_Delete, parametros, commandType: CommandType.StoredProcedure);
+
+    }
 
         public VW_tbCargos Find(int id)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+            parametros.Add("@carg_Id", id, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<VW_tbCargos>(ScriptsDatabase.UDP_Cargo_Find, parametros, commandType: CommandType.StoredProcedure);
+
         }
 
         public RequestStatus Insert(tbCargos item)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+
+            parametros.Add("@carg_Nombre",          item.carg_Nombre,         DbType.String, ParameterDirection.Input);
+            parametros.Add("@carg_UsuarioCreador",  item.carg_UsuarioCreador, DbType.Int32,  ParameterDirection.Input);
+
+            return db.QueryFirst<RequestStatus>(ScriptsDatabase.UDP_Cargo_Insert, parametros, commandType: CommandType.StoredProcedure);
+
         }
 
         public IEnumerable<VW_tbCargos> List()
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+            return db.Query<VW_tbCargos>(ScriptsDatabase.UDP_Cargo_List, null, commandType: CommandType.StoredProcedure);
+
         }
 
         public RequestStatus Update(tbCargos item)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+
+            parametros.Add("@carg_ID ", item.carg_ID, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@carg_Nombre ", item.carg_Nombre, DbType.String, ParameterDirection.Input);
+            parametros.Add("@carg_UsuarioModificador", item.carg_UsuarioModificador, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<RequestStatus>(ScriptsDatabase.UDP_Cargo_Update, parametros, commandType: CommandType.StoredProcedure);
+
         }
     }
 }
--- API/ParqueDiversion/ParqueDiversion.DataAccess/Repositories/Parq/RegionesRepository.cs
+++ API/ParqueDiversion/ParqueDiversion.DataAccess/Repositories/Parq/RegionesRepository.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
 using ParqueDiversion.Entities.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,27 +14,55 @@ namespace ParqueDiversion.DataAccess.Repositories.Parq
     {
         public RequestStatus Delete(int id)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+            parametros.Add("@regi_ID", id, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<RequestStatus>(ScriptsDatabase.UDP_Regiones_Delete, parametros, commandType: CommandType.StoredProcedure);
+
         }
 
         public VW_tbRegiones Find(int id)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+            parametros.Add("@regi_ID", id, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<VW_tbRegiones>(ScriptsDatabase.UDP_Regiones_Find, parametros, commandType: CommandType.StoredProcedure);
+
         }
 
         public RequestStatus Insert(tbRegiones item)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+
+            parametros.Add("@regi_Nombre",         item.regi_Nombre,         DbType.String, ParameterDirection.Input);
+            parametros.Add("@regi_UsuarioCreador", item.regi_UsuarioCreador, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<RequestStatus>(ScriptsDatabase.UDP_Regiones_Insert, parametros, commandType: CommandType.StoredProcedure);
+
         }
 
         public IEnumerable<VW_tbRegiones> List()
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+            return db.Query<VW_tbRegiones>(ScriptsDatabase.UDP_Regiones_List, null, commandType: CommandType.StoredProcedure);
+
         }
 
         public RequestStatus Update(tbRegiones item)
         {
-            throw new NotImplementedException();
+            using var db = new SqlConnection(ParqueDiversionContext.ConnectionString);
+            var parametros = new DynamicParameters();
+
+            parametros.Add("@regi_ID ",                 item.regi_ID, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@regi_Nombre",              item.regi_Nombre, DbType.String, ParameterDirection.Input);
+            parametros.Add("@regi_UsuarioModificador",  item.regi_UsuarioModificador, DbType.Int32, ParameterDirection.Input);
+
+            return db.QueryFirst<RequestStatus>(ScriptsDatabase.UDP_Regiones_Update, parametros, commandType: CommandType.StoredProcedure);
+
         }
     }
 }
