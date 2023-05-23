f7edcf63b7bf14d158c8cd5c00cd7ceb239713a8
aless65 <125475340+itsInfinity03@users.noreply.github.com>
2023-05-23 11:13:32 -0600

    Martes pt2

---
+++ PLANTILLAS/admindashboard/src/app/Models/Regiones.ts
@@ -0,0 +1,14 @@
+export class Regiones{
+    regi_ID!: number;
+    regi_Nombre!: String;
+    regi_Habilitado!: number;
+    regi_Estado!: number;
+    regi_UsuarioCreador!: number;
+    usu_Creador!: String;
+    regi_FechaCreacion!: number;
+    regi_UsuarioModificador!: number;
+    usu_Modificador!: String;
+    regi_FechaModificacion!: String;
+    empl_crea!: String;
+    empl_Modifica!: String    
+}
