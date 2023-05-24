ec7882e8b0c224a4ea5bcfb7c58a1e6c822558aa
★★★  Angie  ★★★ <125337384+jumAngie@users.noreply.github.com>
2023-05-24 11:08:46 -0600

    vamo

--- API/ParqueDiversion/ParqueDiversion.API/obj/Release/net5.0/ParqueDiversion.API.AssemblyInfo.cs
+++ API/ParqueDiversion/ParqueDiversion.API/obj/Release/net5.0/ParqueDiversion.API.AssemblyInfo.cs
@@ -11,7 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ParqueDiversion.API")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("ParqueDiversion.API")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ParqueDiversion.API")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
