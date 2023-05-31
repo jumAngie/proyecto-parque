f00e590958ffeb7fd60a10410ba8a580296118be
★★★  Angie  ★★★ <125337384+jumAngie@users.noreply.github.com>
2023-05-31 14:50:58 -0600

    MAPA

--- PLANTILLAS/admindashboard/src/app/pages/dashboard/dashboard.component.ts
+++ PLANTILLAS/admindashboard/src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Areas } from 'src/app/Models/Areas';
 import { ParqServicesService } from 'src/app/ParqServices/parq-services.service';
 import { Router } from '@angular/router';
 import { Atracciones } from 'src/app/Models/Atracciones';
+import { ToastUtils } from 'src/app/Utilities/ToastUtils';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,7 +20,9 @@ export class DashboardComponent implements OnInit {
   ) {}
   ngOnInit(): void {
     this.cargarAreas();
+    window.addEventListener('resize', this.checkZoomLevel.bind(this));
   }
+  
 
   cargarAreas() {
     this.service.getAreas().subscribe((response: any) => {
@@ -27,6 +30,15 @@ export class DashboardComponent implements OnInit {
         this.areas = response.data;
       }
     });
+  }
+   
+  checkZoomLevel() {
+    var zoomLevel = Math.round(window.devicePixelRatio * 100);
+  
+    if (zoomLevel < 90) {
+      // Mostrar el toast de recomendación
+      ToastUtils.showInfoToast('Para un uso óptimo de los recursos de la aplicación, mantenga el zoom en 90%.');
+    }
   }
 
   cargarAtracciones(AreaId: number){
