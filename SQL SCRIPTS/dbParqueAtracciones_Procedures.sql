5a46db48a18013ab49f4b6baf48d3cf38e980541
Angie Campos <angie.camposyc03@gmail.com>
2023-06-01 21:21:59 -0600

    AÑADI AL MENU EL CATALOGO DE TIPO DE TICKET

--- SQL SCRIPTS/dbParqueAtracciones_Procedures.sql
+++ SQL SCRIPTS/dbParqueAtracciones_Procedures.sql
@@ -3443,5 +3443,7 @@ INSERT [acce].[tbPantallas] ([pant_ID], [pant_Descripcion], [pant_URL], [pant_Me
 GO
 INSERT [acce].[tbPantallas] ([pant_ID], [pant_Descripcion], [pant_URL], [pant_Menu], [pant_HtmlID], [pant_Identificador], [pant_Icono], [pant_Estado], [pant_UsuarioCreador], [pant_FechaCreacion], [pant_UsuarioModificador], [pant_FechaModificacion]) VALUES (9, N'Venta de Tickets', N'/listticketsclientes', NULL, NULL, N'PARQ', N'bi bi-circle', 1, 1, CAST(N'2023-05-30T11:42:17.247' AS DateTime), NULL, NULL)
 GO
+INSERT [acce].[tbPantallas] ([pant_ID], [pant_Descripcion], [pant_URL], [pant_Menu], [pant_HtmlID], [pant_Identificador], [pant_Icono], [pant_Estado], [pant_UsuarioCreador], [pant_FechaCreacion], [pant_UsuarioModificador], [pant_FechaModificacion]) VALUES (10, N'Tipos de Ticket', N'/listtitckets', NULL, NULL, N'PARQ', N'bi bi-circle', 1, 1, CAST(N'2023-05-30T11:42:17.247' AS DateTime), NULL, NULL)
+GO
 SET IDENTITY_INSERT [acce].[tbPantallas] OFF
 GO
