81e7e9ffebafbef157c357800f75cd2fae83b811
★★★  Angie  ★★★ <125337384+jumAngie@users.noreply.github.com>
2023-06-05 14:45:38 -0600

    bueno ya fue

--- PLANTILLAS/admindashboard/src/app/Clientes/listarclientes/listarclientes.component.ts
+++ PLANTILLAS/admindashboard/src/app/Clientes/listarclientes/listarclientes.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ParqServicesService } from 'src/app/ParqServices/parq-services.service';
 
 @Component({
   selector: 'app-listarclientes',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./listarclientes.component.css']
 })
 export class ListarclientesComponent {
+  filtro: String = '';
+  p: number = 1;
+  selectedPageSize = 2;
+  pageSizeOptions: number[] = [2, 4 ,6, 8]; // Opciones de tamaño de página
+
+  constructor(
+    private service: ParqServicesService,
+    private router: Router,
+    private elementRef: ElementRef
+  ) { }
+
 
 }
